dc36d29a4338e9bc5d4ead1daf56eb49fc06c263
Reza Soltani <soltanireza65@gmail.com>
2025-08-15 05:46:32 +0330

    rm unused import

--- packages/correlation-id/src/correlation-id.module.ts
+++ packages/correlation-id/src/correlation-id.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { CorrelationIdService } from './correlation-id.service';
-// import { CorrelationIdMiddleware } from './correlation-id.middleware';
 
 @Module({
   providers: [CorrelationIdService],
